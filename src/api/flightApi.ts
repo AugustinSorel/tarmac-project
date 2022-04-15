3a2ca01e6d3000bf66625c1eb41defc8693ea699
AugustinSorel <sorelaugustin@gmail.com>
2022-04-15 23:14:18 +0200

    key added

--- src/api/flightApi.ts
+++ src/api/flightApi.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { Flight } from "../shared/utils/FlightType";
 
-const REACT_APP_API_URL = process.env.REACT_APP_API_URL as string;
 export const getFlights = async (): Promise<Flight[]> => {
-  const response = await axios.get(`${REACT_APP_API_URL}`);
+  const response = await axios.get(
+    "http://api.aviationstack.com/v1/flights?access_key=d6170c700237b7947acb3f25e57cbd4f"
+  );
   return response.data.data;
 };
